809e699661e96a6897198e4c13ead2191cbfa81d
d.aikema <djurreaikema@gmail.com>
2024-08-01 11:12:10 +0200

    demo

--- NugetDetailsView.xaml.cs
+++ NugetDetailsView.xaml.cs
@@ -1,11 +1,36 @@
-﻿using System.Windows.Controls;
+﻿using System.Reactive.Disposables;
+using System.Windows.Media.Imaging;
+using ReactiveUI;
 
 namespace WpfApp2;
 
-public partial class NugetDetailsView : UserControl
+public partial class NugetDetailsView
 {
     public NugetDetailsView()
     {
         InitializeComponent();
+        this.WhenActivated(disposableRegistration =>
+        {
+            this.OneWayBind(ViewModel,
+                    viewModel => viewModel.IconUrl,
+                    view => view.iconImage.Source,
+                    url => url == null ? null : new BitmapImage(url))
+                .DisposeWith(disposableRegistration);
+
+            this.OneWayBind(ViewModel,
+                    viewModel => viewModel.Title,
+                    view => view.titleRun.Text)
+                .DisposeWith(disposableRegistration);
+
+            this.OneWayBind(ViewModel,
+                    viewModel => viewModel.Description,
+                    view => view.descriptionRun.Text)
+                .DisposeWith(disposableRegistration);
+
+            this.BindCommand(ViewModel,
+                    viewModel => viewModel.OpenPage,
+                    view => view.openButton)
+                .DisposeWith(disposableRegistration);
+        });
     }
 }
