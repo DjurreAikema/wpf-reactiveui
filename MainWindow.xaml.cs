e0c96ebd463d495c6c098f97b3f23648019060ad
d.aikema <djurreaikema@gmail.com>
2024-08-01 11:06:46 +0200

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Reactive.Disposables;
+using ReactiveUI;
+
+namespace WpfApp2;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        ViewModel = new AppViewModel();
+
+        this.WhenActivated(disposableRegistration =>
+        {
+            this.OneWayBind(ViewModel,
+                    viewModel => viewModel.SearchResults,
+                    view => view.SearchResultsListBox.ItemsSource)
+                .DisposeWith(disposableRegistration);
+
+            this.OneWayBind(ViewModel,
+                    viewModel => viewModel.SearchResults,
+                    view => view.SearchResultsListBox.ItemsSource)
+                .DisposeWith(disposableRegistration);
+
+            this.Bind(ViewModel,
+                    viewModel => viewModel.SearchTerm,
+                    view => view.SearchTextBox.Text)
+                .DisposeWith(disposableRegistration);
+        });
+    }
+}
